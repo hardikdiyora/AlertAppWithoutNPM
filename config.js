fab8716b86160158acd95fdc3f1f1b9cae1c4fe7
Hardikkumar Diyora <hardikkumar.diyora@share-now.com>
2021-02-26 08:24:38 +0100

    ft send sms function

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 /**
  * Create and export configuration variables
+ * 
+ * NOTE: This project is purely for learning purpose so you will see that some secret information in config.js or
+ *  https certificates are exposed in code repository publicly. 
+ *  Ideally, this will be kept as environment variables or using some infrastructure config deployment. 
  */
 
 const { type } = require("os")
@@ -8,12 +12,18 @@ const { type } = require("os")
 let environments = {}
 
 // Staging (default) environment
+
 environments.staging = {
     'httpPort': 3000,
     'httpsPort': 3001,
     'envName': 'Staging',
     'hashingSecret': 'thisIsASecret',
-    'maxChecks': 5
+    'maxChecks': 5,
+    'twilio': {
+        'accountSid': 'ACb32d411ad7fe886aac54c665d25e5c5d',
+        'authToken': '9455e3eb3109edc12e3d8c92768f7a67',
+        'fromPhone':'+15005550006'
+    }
 }
 
 
